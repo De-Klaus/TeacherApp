667346c792fde2c11fc5c3f056d3dbb5453325d3
petko_isands <petko@isands.ru>
2025-02-02 02:09:14 +0700

    fix add Teacher

--- src/main/java/org/teacher/TeacherApplication.java
+++ src/main/java/org/teacher/TeacherApplication.java
@@ -5,15 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.teacher.model.Teacher;
 import org.teacher.repository.TeacherRepository;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EntityScan("org.teacher.model")
+@EnableJpaRepositories("org.teacher.repository")
 public class TeacherApplication {
 
     public static void main(String[] args) {
