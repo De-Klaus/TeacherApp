32bc52775efef60aa86da387d19efaba2916fa0c
petko_isands <petko@isands.ru>
2025-02-08 19:20:03 +0700

    add org.springframework:spring-web

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,16 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework:spring-web")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.security:spring-security-config")
+    implementation("org.springframework.security:spring-security-web")
+
+    // Шифрование паролей
+    implementation("org.springframework.security:spring-security-crypto")
+    implementation("org.springframework:spring-webmvc")
 
     implementation("org.postgresql:postgresql")
     implementation("org.projectlombok:lombok")
