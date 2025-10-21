3886368bea85cd431238a4808e25f1d346e78bd7
petko_isands <petko@isands.ru>
2025-10-21 23:38:09 +0700

    [ref] change statuses describe

--- src/main/java/org/teacher/entity/StudentStatus.java
+++ src/main/java/org/teacher/entity/StudentStatus.java
@@ -4,9 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum StudentStatus {
-    CREATED_BY_SYSTEM("Создан админом или учителем", "primary"),
+    CREATED_BY_SYSTEM("Ученик без аккаунта", "primary"),
     REGISTERED("Пользователь привязался", "warning"),
-    ACTIVE("Участвует в занятиях", "success"),
+    ACTIVE("Участвует в занятиях с аккаунтом", "success"),
     INACTIVE("Неактивен", "danger");
 
     private final String text;
